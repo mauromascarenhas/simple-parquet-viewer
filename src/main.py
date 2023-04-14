1b9bde6bc7bc9e55907a65c5d55294bae5d88bdc
Mauro Mascarenhas de Araújo <mauromascarenhas@users.noreply.github.com>
2023-04-13 22:47:19 -0300

    Translator support
    - Add support to application translation (QTranslator).

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import os
 import sys
 
+from PyQt6.QtCore import QTranslator, QLocale
 from PyQt6.QtWidgets import QApplication
+
 from widgets.mainwindow import MainWindow
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -9,6 +11,11 @@ os.environ["SPV_SD_"] = SCRIPT_DIR
 
 def main():
     app = QApplication(sys.argv)
+    translator = QTranslator()
+
+    if translator.load(QLocale(), os.path.join(SCRIPT_DIR, "res", "translations") + os.path.sep):
+        app.installTranslator(translator)
+
     app.setApplicationName("Simple Parquet Viewer")
     with open(os.path.join(SCRIPT_DIR, "res", "stylesheet", "dark.css"), "r") as f: app.setStyleSheet(f.read())
 
