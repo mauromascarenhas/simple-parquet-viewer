efc9f26c0a4980a9de987d1183cca614491e681b
Mauro Mascarenhas de Araújo <mauromascarenhas@users.noreply.github.com>
2023-04-15 15:22:11 -0300

    Shortcut creation script
    - New script for creation of application shortcut (Windows only):
        - Python implementation in "main" file;
        - Script entry in pyproject.toml file.
    - Added optional dependencies entry to pyproject.toml.

--- src/simple_parquet_viewer/main.py
+++ src/simple_parquet_viewer/main.py
@@ -33,4 +33,26 @@ def main():
     
     sys.exit(app.exec())
 
+def gen_shortcut():
+    if os.name != "nt":
+        print("This feature is only available for Windows.")
+        sys.exit(0)
+
+    out = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.path.expandvars("%APPDATA%"), "Microsoft", "Windows", "Start Menu", "Programs")
+    if not (os.path.exists(out) and os.path.isdir(out)):
+        print(f"A valid directory must be provided: \"{out}\" is not a directory.")
+        sys.exit(1)
+    
+    try:
+        from shutil import which
+        from win32com.client import Dispatch
+    except:
+        print("There was an error while importing the necessary dependencies. Please, certify that 'pywin32' package is installed.")
+        sys.exit(1)
+
+    sc = Dispatch("WScript.Shell").CreateShortCut(os.path.join(out, "Simple Parquet Viewer.lnk"))
+    sc.Targetpath = which("spv")
+    sc.IconLocation = os.path.join(SCRIPT_DIR, "res", "imgs", "app_icon.ico")
+    sc.save()
+
 if __name__ == "__main__": main()
